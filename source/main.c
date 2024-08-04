fdc48e46a330fee7334389485237d33a4ad45cc9
amurph <aidanpmurphy@hotmail.com>
2024-08-03 17:08:02 -0700

    start from copied template

--- source/main.c
+++ source/main.c
@@ -2,6 +2,7 @@
 #include "server.h"
 
 int main(int argc, char *argv[]) {
+	printf("entering open_server()...\n");
 	open_server();
-	printf("----\n%s with %d args", argv[0], argc);
+	printf("----\n%s with %d args\n", argv[0], argc);
 }
